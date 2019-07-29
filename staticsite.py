4e9e4523303443aa096e9f37f155bee357fd8fae
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-07-28 20:56:28 -0700

    Started work on static site.

---
+++ staticsite.py
@@ -0,0 +1,13 @@
+
+# Scaffold a simple, static website
+"""
+# Directory and file structure to scaffold
+-src
+ |__/css
+ |  |_main.css
+ |__/img
+ |__/js
+ |    |_main.js
+ |__index.html
+ |__README.md
+"""
