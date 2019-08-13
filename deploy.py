0b1e58b015f99dd2da3eb25082f04d637149636e
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-08-13 13:17:43 -0700

    Removed unworking and redundant code

--- deploy.py
+++ deploy.py
@@ -7,27 +7,4 @@ from shutil import move
 
 
 # Run cmds to compile for both Windows & Linux
-def compile():
-
-    call(['pyinstaller', 'main.py', '-F', '-n', 'scaffold_projects'])
-    call(['wsl', 'pyinstaller', 'main.py', '-F', '-n', 'scaffold_projects'])
-
-
-# Move all executables to a specific dir after being created
-def move_executables():
-
-    # Default location, created by PyInstaller
-    src_dir = getenv('SCAFF_DIST')
-    # Define a location to move all executables to 
-    target_dir = getenv('EXEC_DIR')
-
-    move(src_dir, target_dir)
-
-
-def deploy():
-
-    compile()
-    move_executables()
-
-
-deploy()
+call(['pyinstaller', 'main.py', '-F', '-n', 'scaffold_projects'])
