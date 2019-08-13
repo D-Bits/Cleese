953ac2f7674ab3cec8deba0fd42c988981f7d449
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-08-13 13:23:09 -0700

    Misc changes

--- deploy.py
+++ deploy.py
@@ -2,9 +2,8 @@
 Run this file automate compiling of execuatables.
 """
 from subprocess import call
-from os import getenv
-from shutil import move
 
 
 # Run cmds to compile for both Windows & Linux
 call(['pyinstaller', 'main.py', '-F', '-n', 'scaffold_projects'])
+call(['wsl', 'pyinstaller', 'main.py', '-F', '-n', 'scaffold_projects'])
